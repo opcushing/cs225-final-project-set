2f547cd3a095e056ad028df376c848f9480f2a4f
Owen Cushing <opcushing@gmail.com>
2022-11-23 20:48:36 -0600

    wikigraph function outline

--- src/driver.cc
+++ src/driver.cc
@@ -2,6 +2,23 @@
 
 #include "utilities.hpp"
 
+#include "wikigraph.hpp"
+
 int main(int argc, char* argv[]) {
+  // TODO: Setup I/O
+
+  (void) argc;
+  (void) argv;
+  
+  /**
+   * This should likely take in the filename of our data as input.
+   * Using flags, we can request what the user would like to determine
+   *  - Need to be able to request BFS search of path for the time being.
+   * We should construct helper functions in order to clean / parse the data.
+   * Then construct the WikiGraph from the data.
+   * Call BFS search on it from the given input
+   *  - Maybe -f [from_page] -t [to_page]
+  */
+
   return 0;
 }
