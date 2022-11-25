c1ddb307a69d7d4b105ae5f06682fa6021008926
Owen Cushing <opcushing@gmail.com>
2022-11-25 17:38:38 -0600

    copied mp_schedule utility helpers.

--- src/utilities.cc
+++ src/utilities.cc
@@ -1,6 +1,50 @@
 #include "utilities.hpp"
+#include <sstream>
+#include <fenv.h>
+#include <signal.h>
+#include <vector>
+
 
 // TODO: DecodeURL()
-std::string DecodeURL(const std::string& urL_str) {
-  return urL_str;
+std::string DecodeURL(const std::string& url_str) {
+  
+  return url_str;
+}
+
+// All code below taken from CS225 mp_schedule utils.cc
+
+std::string file_to_string(const std::string& filename){
+  std::ifstream text(filename);
+
+  std::stringstream strStream;
+  if (text.is_open()) {
+    strStream << text.rdbuf();
+  }
+  return strStream.str();
+}
+
+std::string TrimRight(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(tmp.find_last_not_of(" ") + 1);
+}
+
+std::string TrimLeft(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(0, tmp.find_first_not_of(" "));
+}
+
+std::string Trim(const std::string & str) {
+    std::string tmp = str;
+    return TrimLeft(TrimRight(str));
+}
+
+int SplitString(const std::string & str1, char sep, std::vector<std::string> &fields) {
+    std::string str = str1;
+    std::string::size_type pos;
+    while((pos=str.find(sep)) != std::string::npos) {
+        fields.push_back(str.substr(0,pos));
+        str.erase(0,pos+1);  
+    }
+    fields.push_back(str);
+    return fields.size();
 }
