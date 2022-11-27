09a933fa79dcc17f2f3d67ba051a5f15b5d18a4b
Owen Cushing <opcushing@gmail.com>
2022-11-26 18:29:39 -0600

    removed print statements. fixed style issues.

--- src/wikigraph.cc
+++ src/wikigraph.cc
@@ -12,15 +12,13 @@ WikiGraph::WikiGraph(const std::string& file_name) {
   // Populates map.
 
   // open file
-  // get a line; decode the first item, decode the second item, add the second
-  // to adjlist of firsts
+  // get a line; decode the first item, decode the second item, add the second to adjlist of firsts
   std::string file = file_to_string(file_name);
   std::vector<std::string> lines;
   SplitString(file, '\n', lines);
-  lines
-      .pop_back();  // our dataset has a line of empty space that we can discard
+  lines.pop_back(); // our dataset has a line of empty space that we can discard
 
-  std::unordered_map<std::string, std::string> decoded;  // memoize decoding
+  std::unordered_map<std::string, std::string> decoded; // memoize decoding
   for (auto& line : lines) {
     std::vector<std::string> pages;
     SplitString(line, '\t', pages);
@@ -44,8 +42,6 @@ WikiGraph::WikiGraph(const std::string& file_name) {
 // TODO: getPathBFS()
 std::vector<std::string> WikiGraph::getPathBFS(
     const std::string& start_page, const std::string& end_page) const {
-  std::cout << "Finding BFS Path from [" << start_page << "] to [" <<
-    end_page << "]" << std::endl;
 
   std::vector<std::string> page_path;
 
@@ -57,10 +53,8 @@ std::vector<std::string> WikiGraph::getPathBFS(
 
   while (!to_visit.empty() && curr_page != end_page) {
     curr_page = to_visit.front();
-    // std::cout << "curr_page: " << curr_page << std::endl;
     to_visit.pop();
     if (article_map.find(curr_page) != article_map.end()) {
-      // std::cout << "AH" << std::endl;
       std::vector<std::string> adjacent_pages = article_map.at(curr_page);
       for (const auto& adj_page : adjacent_pages) {
         // if page hasn't been visited.
@@ -69,7 +63,7 @@ std::vector<std::string> WikiGraph::getPathBFS(
           page_tree[adj_page] = curr_page;
         }
         if (adj_page == end_page) {
-          break;  // path to page was found.
+          break; // path to page was found.
         }
       }
     }
@@ -77,11 +71,8 @@ std::vector<std::string> WikiGraph::getPathBFS(
 
   // Path between pages was not found...
   if (page_tree.count(end_page) == 0) {
-    std::cout << "NO PATH FOUND" << std::endl;
-    return page_path;
+    throw std::runtime_error("Path to page not found");
   }
-
-  std::cout << "Constructing Path..." << std::endl;
 
   std::string temp_page = end_page;
   std::vector<std::string> reverse_path;
@@ -100,11 +91,11 @@ std::vector<std::string> WikiGraph::getPathBFS(
 
 // TODO: getPathDijkstras()
 std::vector<std::string> WikiGraph::getPathDijkstras(
-    const std::string& start_page, const std::string& end_page) const {
+    const std::string& from_page, const std::string& to_page) const {
   std::vector<std::string> path_to_return;
   // ...
-  (void)start_page;
-  (void)end_page;
+  (void) from_page;
+  (void) to_page;
 
   return path_to_return;
 }
