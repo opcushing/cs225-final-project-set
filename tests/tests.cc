7fdf23623e641e5cfd7dab644f05241c2c4b2ce9
jamieRollison <rollisonjamie15@gmail.com>
2022-11-27 13:18:09 -0600

    added test coverage for BFS

--- tests/tests.cc
+++ tests/tests.cc
@@ -49,3 +49,27 @@ TEST_CASE("Dijkstra's picks the best path", "dijkstras") {
   REQUIRE(expectedAE == w.getPathDijkstras("A", "E"));
   REQUIRE(expectedAD == w.getPathDijkstras("A", "D"));
 }
+
+TEST_CASE("BFS simple", "BFS") {
+  std::vector<std::string> expectedAE = {
+    "A", "B", "C", "D", "E"
+  };
+  std::vector<std::string> expectedCD = {
+    "C", "D"
+  };
+  WikiGraph w("./datasets/line_graph.tsv");
+  REQUIRE(expectedAE == w.getPathBFS("A", "E"));
+  REQUIRE(expectedCD == w.getPathBFS("C", "D"));
+}
+
+TEST_CASE("BFS picks the best path", "BFS") {
+  std::vector<std::string> expectedAE = {
+    "A", "E"
+  };
+  std::vector<std::string> expectedAD = {
+    "A", "B", "D"
+  };
+  WikiGraph w("./datasets/diff_paths.tsv");
+  REQUIRE(expectedAE == w.getPathBFS("A", "E"));
+  REQUIRE(expectedAD == w.getPathBFS("A", "D"));
+}
