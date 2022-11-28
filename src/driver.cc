d585a7b764d998e8804d7124f8e629b9b7baf7f9
jamieRollison <rollisonjamie15@gmail.com>
2022-11-28 11:46:53 -0600

    ui done mostly

--- src/driver.cc
+++ src/driver.cc
@@ -6,27 +6,55 @@
 
 int main(int argc, char* argv[]) {
   // TODO: Setup I/O
-
-  (void) argc;
-  (void) argv;
-  
   /**
-   * This should likely take in the filename of our data as input.
-   * Using flags, we can request what the user would like to determine
-   *  - Need to be able to request BFS search of path for the time being.
-   * We should construct helper functions in order to clean / parse the data.
-   * Then construct the WikiGraph from the data.
-   * Call BFS search on it from the given input
-   *  - Maybe -f [from_page] -t [to_page]
+   * Usage: algorithm flag, start page, end page
+   * ex -b "start" "end" (BFS)
+   *    -d "start" "end" (Dijkstra's)
+   *    -p "start" "end" (PageRank)
   */
+  if (!(argc == 4 || (argc == 3 && argv[1] == "-p"))) {
+    std::cout << "Usage: algorithm flag, start page, end page\n"
+                << "or -p n for the first n pages"
+                << "\t-b \"start\" \"end\" (BFS)\n"
+                << "\t-d \"start\" \"end\" (Dijkstra's)\n"
+                << "\t-p n (PageRank)" << std::endl;
+    return 1;
+  }
+
   WikiGraph w{"./datasets/links.tsv"};
 
-  std::vector<std::string> page_path = w.getPathBFS("Advertising", "Silicon");
-
-  for (const auto& page : page_path) {
-    std::cout << page << " -> ";
+  std::vector<std::string> path;
+  
+  // pick an algorithm
+  switch (argv[1][1]) {
+    case 'b': {
+      path = w.getPathBFS(argv[2], argv[3]);
+      break;
+    }
+    case 'd': {
+      path = w.getPathDijkstras(argv[2], argv[3]);
+      break;
+    }
+    case 'p': {
+      // need to figure out what this does
+      auto pages = w.rankPages();
+      // print the first n pages
+      break;
+    }
+    default: {
+      std::cout << "Invalid algorithm" << std::endl;
+      std::cout << "Usage: algorithm flag, start page, end page\n"
+                << "\t-b \"start\" \"end\" (BFS)\n"
+                << "\t-d \"start\" \"end\" (Dijkstra's)\n"
+                << "\t-p \"start\" \"end\" (PageRank)" << std::endl;
+      return 1;
+    }
   }
-  std::cout << "[Finished]" << std::endl;
+
+  for (auto i = path.begin(); i != path.end() - 1; ++i) {
+    std::cout << *i << " -> ";
+  }
+  std::cout << path.back() << std::endl;
 
   return 0;
 }
