f4893ae3eacd4cd2fecb459f3fbb4734444f4c48
Owen Cushing <opcushing@gmail.com>
2022-12-07 16:45:21 -0600

    brandes process is parallelized.

--- includes/wikigraph.hpp
+++ includes/wikigraph.hpp
@@ -3,8 +3,50 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
 
 typedef std::map<std::string, std::vector<std::string>> Graph;
+
+class SafeQueue {
+  mutable std::mutex mutex_;
+  std::queue<std::string> q_;
+  bool empty() const {
+    return q_.empty();
+  }
+  public:
+  SafeQueue() = default;
+  SafeQueue(const SafeQueue&) = delete;
+  SafeQueue& operator=(const SafeQueue&) = delete;
+
+  SafeQueue(SafeQueue&& other) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    q_ = std::move(other.q_);
+  }
+ 
+  virtual ~SafeQueue() { }
+ 
+  unsigned long size() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return q_.size();
+  }
+ 
+  std::optional<std::string> pop() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (q_.empty()) {
+      return {};
+    }
+    std::string tmp = q_.front();
+    q_.pop();
+    return tmp;
+  }
+ 
+  void push(const std::string &item) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    q_.push(item);
+  }
+};
 
 class WikiGraph {
 public:
@@ -27,6 +69,10 @@ public:
   double getBetweenCentrality(const std::string& page) const;
 
   std::map<std::string, double> getCentralityMap() const;
+
+  void threadHelper(SafeQueue& q, std::map<std::string, double>& centrality_map, const std::vector<std::string>& pages) const;
+
+  void brandesHelper(const std::string& start, std::map<std::string, double>& centrality_map, const std::vector<std::string>& pages) const;
 
 
   // ------ Helpers ---------
